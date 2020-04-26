73da4ee8e64648ec93f0f1d22feed6963c233eca
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2020-04-26 18:59:36 -0400

    created description component

---
+++ src/components/CollectionDescription.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <p class="text-h3 text-center">{{info.name}}</p>
+    <q-markdown :no-line-numbers="$store.getters['displaySettings/getDisableLineNumbers']">
+      {{sanitizeInfo(info.description)}}
+    </q-markdown>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'collectionDescription',
+  props: ['info'],
+  data () {
+    return {}
+  },
+  methods: {
+    sanitizeInfo (info) {
+      if (info) {
+        return info.toString().trim()
+      }
+    }
+  }
+}
+</script>
