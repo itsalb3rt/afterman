d4e1bd0c6b75f3dea6006c208bc8ff5e00ab7aff
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2020-04-25 23:55:14 -0400

    created components

---
+++ src/components/RequestBody.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <q-markdown :no-line-numbers="true">
+      {{parseDescription(body)}}
+    </q-markdown>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'requestBody',
+  props: ['body'],
+  data () {
+    return {}
+  },
+  methods: {
+    parseDescription (description) {
+      return description.toString()
+    }
+  }
+}
+</script>
---
+++ src/components/RequestMethod.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <q-chip :color="getColorByMethod(method)" text-color="white">
+      {{ method }}
+    </q-chip>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'requestMethod',
+  props: ['method'],
+  data () {
+    return {
+      colors: [
+        { method: 'POST', value: 'primary' },
+        { method: 'GET', value: 'positive' },
+        { method: 'PATCH', value: 'grey' },
+        { method: 'DELETE', value: 'negative' },
+        { method: 'PUT', value: 'indigo' }
+      ]
+    }
+  },
+  methods: {
+    getColorByMethod (method) {
+      const result = this.colors.find(color => color.method === method.toUpperCase())
+      return result.value
+    }
+  }
+}
+</script>
