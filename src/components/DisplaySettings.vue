487fdbda36469229ed366821160b5a6a53f52ae4
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2020-04-26 00:16:59 -0400

    added diplay line setting

--- src/components/DisplaySettings.vue
+++ src/components/DisplaySettings.vue
@@ -1,12 +1,40 @@
 <template>
-  <div>My component</div>
+  <div>
+    <q-page-sticky position="bottom-right" :offset="[18, 18]">
+      <q-btn fab icon="settings" color="primary" @click="toggleDialog = true" />
+    </q-page-sticky>
+    <q-dialog position="right" v-model="toggleDialog">
+      <q-card>
+        <q-card-section class="row items-center q-pb-none">
+          <div class="text-h6">Display settings</div>
+          <q-space />
+          <q-btn icon="close" flat round dense v-close-popup />
+        </q-card-section>
+
+        <q-card-section>
+          Controls the general display settings
+        </q-card-section>
+        <q-card-section>
+          <q-toggle @input="toggleDisableLineNumbers()" v-model="toggleLineNumbers" label="Disable line numbers"></q-toggle>
+        </q-card-section>
+      </q-card>
+    </q-dialog>
+  </div>
 </template>
 
 <script>
 export default {
-  // name: 'ComponentName',
+  name: 'displaySettings',
   data () {
-    return {}
+    return {
+      toggleDialog: false,
+      toggleLineNumbers: false
+    }
+  },
+  methods: {
+    toggleDisableLineNumbers () {
+      this.$store.commit('displaySettings/SET_DISABLE_LINE_NUMBER', this.toggleLineNumbers)
+    }
   }
 }
 </script>
