12af2635912452e0f0d19173dc3230928333bf79
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2020-04-25 21:13:55 -0400

    added markdown extension

--- quasar.conf.js
+++ quasar.conf.js
@@ -13,7 +13,7 @@ module.exports = function (ctx) {
     // --> boot files are part of "main.js"
     // https://quasar.dev/quasar-cli/cli-documentation/boot-files
     boot: [
-
+      'qmarkdown',
       'i18n'
     ],
 
