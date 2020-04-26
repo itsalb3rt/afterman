0fe9747fbe2bb330c74ac5d9a29fc5839074bf44
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2020-04-26 19:00:17 -0400

    created request URL component

---
+++ src/components/RequestUrl.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <q-markdown>
+      {{ setUrlVars(url) }}
+    </q-markdown>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'requestUrl',
+  props: ['url'],
+  data () {
+    return {}
+  },
+  methods: {
+    setUrlVars (url) {
+      const urlVars = url.match(/({{[a-zA-Z]+}})/)
+      urlVars.forEach(element => {
+        const result = this.$store.getters['collection/getCollectionVariables'].find(item => item.key === element.replace(/({{)|(}})/g, ''))
+        url = url.replace(`{{${result.key}}}`, result.value)
+      })
+      return url
+    }
+  }
+}
+</script>
